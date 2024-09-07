b4c584a578c8c46ddd8396d0f900bf43400da155
Darze <reb@hotmail.gr>
2024-09-07 16:05:05 +0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { MainPanelComponent } from './main-panel/main-panel.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    SearchBarComponent,
+    MainPanelComponent,
+    CategoriesComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'hall-of-fame-app';
---
+++ src/app/main-panel/main-panel.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-panel',
+  standalone: true,
+  templateUrl: './main-panel.component.html',
+  styleUrl: './main-panel.component.scss',
+})
+export class MainPanelComponent {}
