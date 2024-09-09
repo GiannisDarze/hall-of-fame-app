cf56bd68ebc8fb73d7f6ead6fae2b85420fd0a46
Darze <reb@hotmail.gr>
2024-09-09 23:39:44 +0300

    Re-push because I forgot to push some other changes

--- src/app/main-panel/main-panel.component.ts
+++ src/app/main-panel/main-panel.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
-
+import { MetalArchivesApiService } from '../services/metal-archives-api.service';
 @Component({
   selector: 'app-main-panel',
   standalone: true,
   templateUrl: './main-panel.component.html',
   styleUrl: './main-panel.component.scss',
 })
-export class MainPanelComponent {}
+export class MainPanelComponent {
+  constructor(private metalArchives: MetalArchivesApiService) {}
+
+  getStuff() {
+    this.metalArchives.getAlbum$('1218049').subscribe({
+      next: (data) => console.log('Album data:', data), // Logs the data fetched
+      error: (error) => console.error('Error fetching album data:', error),
+    });
+  }
+}
